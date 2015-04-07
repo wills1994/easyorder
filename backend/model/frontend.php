a58762e949531f7d7143ef218a28418209b4aab6
Arkaitz Garro <arkaitz.garro@gmail.com>
2015-04-07 10:28:20 +0200

    Refactorizar directorios

--- backend/model/frontend.php
+++ backend/model/frontend.php
@@ -15,25 +15,25 @@ if(isset($_POST['id_mesa'],$_POST['estado'])){
 		$result = mysql_query($query) or handleDBerror();
 	}
 	updateEstadoMesa($id_mesa,$estado);
-    
+
 }else if(isset($_POST["id_mesa"],$_POST["pedido"],$_POST['total'])){
 	$id_mesa=$_POST['id_mesa'];
     $arrayPedido = $_POST['pedido'];
     $total=$_POST['total'];
-    
+
     //INGRESAR EL PEDIDO A LA TABLA "PEDIDOS"
     $query="INSERT INTO `pedido` (id_mesa,total) VALUES ($id_mesa,$total)";
     $result = mysql_query($query) or handleDBerror();
-    
+
     //RECUPERAR EL ID DEL PEDIDO INGRESADO (PORQUE LO GENERA AUTOMATICAMENTE MYSQL)
     $id_pedido = mysql_insert_id();
 
     //INGRESAR PRODUCTOS A LA TABLA "PRODUCTO_HAS_PEDIDO"
     foreach ($arrayPedido as $producto) {
         $query="INSERT INTO `producto_has_pedido` (id_producto, id_pedido, cantidad) VALUES (".$producto[id].",".$id_pedido.",".$producto[cantidad].")";
-        $result = mysql_query($query) or handleDBerror();         
-    }    
-    
+        $result = mysql_query($query) or handleDBerror();
+    }
+
 }else if(isset($_POST["selectmesa"])){
 	$query = "SELECT * FROM mesa";
 	consulta($query);
@@ -59,8 +59,8 @@ else{
 function consulta($sql){
 	$vacio=false;
 	$result = mysql_query($sql) or handleDBerror();
-		
-		if(mysql_num_rows($result) == 0){ 
+
+		if(mysql_num_rows($result) == 0){
 			$vacio=false;
 			echo json_encode($vacio);
 		}else{
@@ -68,7 +68,7 @@ function consulta($sql){
 			while($row = mysql_fetch_object($result)){
 				$array[] = $row;
 			}
-			echo json_encode($array);	
+			echo json_encode($array);
 		}
 }
 ?>
--- js/mvc.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-
-$.getScript("js/model/model.js")
-.done($.getScript("js/view/view.js"))
-.done($.getScript("js/controller/controller.js"));
-
-});//acaba document ready
